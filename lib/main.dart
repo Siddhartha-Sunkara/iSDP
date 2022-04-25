f8ba3371cbbb44b401a4b7a8737e2f3211042880
Siddhartha-Sunkara <siddhartha.sunkara20@st.niituniversity.in>
2022-04-26 02:36:01 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:project2/page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Myapp',
+      home:mainPage(),
+    );
+  }
+}
+
